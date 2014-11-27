a18674c18b4865152677e147ded812110a0efa02
juan2ramos <juan2ramos@gmail.com>
2014-11-27 17:30:17 -0500

    no message

--- wp-content/themes/cb-modello/header.php
+++ wp-content/themes/cb-modello/header.php
@@ -127,7 +127,7 @@ if (isset($cb_type)) {
                        <div class="response" id="mce-success-response" style="display:none"></div>
                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                    <div style="position: absolute; left: -5000px;"><input type="text" class="btn-mail" name="b_811b26cc0fbe5c4e316fe7d70_0d1ade0c88" tabindex="-1" value=""></div>
-                   <div class="clear"><input type="submit" value="Registrate" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                   <div class="clear"><input type="submit" value="Registrate" name="subscribe" id="mc-embedded-subscribe" class="btn-mail"></div>
                </div>
            </form>
        </div>
