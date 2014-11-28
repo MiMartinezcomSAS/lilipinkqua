46ab656054a1d8f47b49a0c7473a767171416902
Michael Martinez <soporte@mi-martinez.com>
2014-11-27 19:28:05 -0500

    Analitycs Integracion

--- wp-content/themes/cb-modello/header.php
+++ wp-content/themes/cb-modello/header.php
@@ -63,6 +63,16 @@ $cb_type = esc_attr(get_post_meta($post->ID, '_cb5_post_type', 'true'));
         wp_head();
         ?>
         <link rel="shortcut icon" href="http://lilipink.com/wp-content/uploads/2014/10/favicon.ico">
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52560577-11', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </head>
 <?php $war = '';
 if (isset($cb_type)) {
