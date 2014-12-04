b42f0822f634f1ae025857af0032549bf54aa0a5
juan2ramos <juan2ramos@gmail.com>
2014-12-04 12:59:33 -0500

    ;

--- wp-content/themes/cb-modello/functions.php
+++ wp-content/themes/cb-modello/functions.php
@@ -1544,6 +1544,6 @@ add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return 
 add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
 add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
 # WordPress 3.x:
-add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) )
+add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
 
 
