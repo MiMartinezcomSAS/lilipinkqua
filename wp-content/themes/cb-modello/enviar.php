454b8cbe20b1f0bf450fe334d3ad8f7cced2c748
chiricos <edwarddiaz92@gmail.com>
2014-12-05 15:56:14 -0500

    configurando que va en el correo de contactenos

--- wp-content/themes/cb-modello/enviar.php
+++ wp-content/themes/cb-modello/enviar.php
@@ -40,7 +40,7 @@
 		$headers = 'From: '.$email_from."\r\n".
 		'Reply-To: '.$email_from."\r\n" .
 		'X-Mailer: PHP/' . phpversion();
-		mail("edwarddiaz92@gmail.com", $mensajeenviar, $headers);
+		mail("edwarddiaz92@gmail.com","envio formulario contactenos", $mensajeenviar, $headers);
 		$mensajecorreo=true;
 	}
 ?>
