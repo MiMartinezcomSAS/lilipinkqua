231cf8b9f4a1689c1f673dea1c92562b98821ffb
chiricos <edwarddiaz92@gmail.com>
2014-12-05 16:03:07 -0500

    viendo el contenido del  msn

--- wp-content/themes/cb-modello/enviar.php
+++ wp-content/themes/cb-modello/enviar.php
@@ -4,6 +4,7 @@
     $ciudad=$_POST["ciudad"];
     $mensaje=$_POST["mensaje"];
     $mensajeenviar= "nombre: ".$nombre."\n correo: ".$correo."\n ciudad: ".$ciudad."\n mensaje: ".$mensaje."";
+    $enviarcorreo=wordwrap($message, 300, "\n");
     $mensajecorreo=false;
     $errorMessage= array( false,false,false,false,);
     $error=false;
@@ -40,7 +41,7 @@
 		$headers = 'From: '.$email_from."\r\n".
 		'Reply-To: '.$email_from."\r\n" .
 		'X-Mailer: PHP/' . phpversion();
-		mail("edwarddiaz92@gmail.com","envio formulario contactenos", $mensajeenviar, $headers);
+		mail("edwarddiaz92@gmail.com","envio formulario contactenos", $enviarcorreo, $headers);
 		$mensajecorreo=true;
 	}
 ?>
